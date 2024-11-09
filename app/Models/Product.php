17b05ed57a4ee9c94e6567713bc99cc670dcb5e3
dinh phuc tuan nhat <nhat18349276@gmail.com>
2024-11-10 01:59:04 +0700

    upgrade home

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,11 @@ class Product extends Model
 
     public function Tag(){
         return $this->belongsToMany(Tag::class,'product_tag','product_id','tag_id')->withPivot('end_date');
+    }
+    public function scopeProductWithTag($query, $tag_name){
+        return $query->whereHas('Tag',function($query) use($tag_name){
+            $query->where('tags.name', $tag_name)
+            ->where("product_tag.end_date",">",Carbon::now());
+        });
     }
 }
